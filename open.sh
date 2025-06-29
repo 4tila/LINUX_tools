2ba0ac2766d2ef0a224be376b4690d8e35c55014
4tila <loboatila@gmail.com>
2025-06-29 10:35:29 -0300

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,49 +1,56 @@
-IN=$(fzf)
-a=(${IN//\./ })
-x=${#a[@]}-1
-flag=true
-if [ ${a[$x]} == "mobi" ]
-then
-    flag=false
-    calibre "$IN"
-fi
-if [ ${a[$x]} == "md" ]
-then
-    flag=false
-    typora "$IN"
-fi
-if [ ${a[$x]} == "bmp" ] || [ ${a[$x]} == "pdf" ] || [ ${a[$x]} == "djvu" ] || [ ${a[$x]} == "jpg" ] || [  ${a[$x]} == "png" ] || [   ${a[$x]} == "jpeg" ] ||  [   ${a[$x]} == "cbz" ] ||  [   ${a[$x]} == "cbr" ]
-then
-    flag=false
-    xdg-open "$IN"
-fi
-if [ ${a[$x]} == "gz" ] || [ ${a[$x]} == "tar" ]
-then
-    flag=false
-    tar -xzvf "$IN"
-fi
-if [ ${a[$x]} == "tex" ]
-then
-    flag=false
-    gummi "$IN"
-fi
-if [ ${a[$x]} == "wav" ]
-then
-    flag=false
-    mpv "$IN"
-fi
-if [ ${a[$x]} == "lyx" ]
-then
-    flag=false
-    lyx "$IN"
-fi
-if [ ${a[$x]} == "mp4" ] || [  ${a[$x]} == "mp3" ] || [ ${a[$x]} == "mkv" ]
-then
-    flag=false
-    cvlc "$IN"
+#!/usr/bin/env zsh
+
+## Open's the file and also adds the command to open the file to clipboard
+## so that you just paste the command to terminal (shift+insert for me) in case
+## you need to do it again
+
+IN=$(ls | fzf) || exit 1  # Exit if user cancels fzf
+
+if [[ -z "$IN" ]]; then
+    exit 1
 fi
 
-if [ $flag == true ]
-then
-    gnome-terminal -- vim "$IN"
-fi
+ext="${IN##*.}" # the ## removes the longest string match of *. and # the shortest
+
+case "$ext" in
+    mobi) 
+        calibre "$IN"
+        echo -n "calibre \"$IN\"" | xclip -selection primary
+        ;;
+    md) 
+        typora "$IN"
+        echo -n "typora \"$IN\"" | xclip -selection primary
+        ;;
+    bmp|pdf|djvu|jpg|png|jpeg|cbz|cbr|eps) 
+        xdg-open "$IN"
+        echo -n "xdg-open \"$IN\"" | xclip -selection primary
+        ;;
+    gz|tar) 
+        tar -xzvf "$IN"
+        echo -n "tar -xzvf \"$IN\"" | xclip -selection primary
+        ;;
+    tex) 
+        gummi "$IN"
+        echo -n "gummi \"$IN\"" | xclip -selection primary
+        ;;
+    wav) 
+        mpv "$IN"
+        echo -n "mpv \"$IN\"" | xclip -selection primary
+        ;;
+    lyx) 
+        lyx "$IN"
+        echo -n "lyx \"$IN\"" | xclip -selection primary
+        ;;
+    mp4|mp3|mkv) 
+        cvlc "$IN"
+        echo -n "cvlc \"$IN\"" | xclip -selection primary
+        ;;
+    py)
+        python3 "$IN"
+        echo -n "python3 \"$IN\"" | xclip -selection primary
+        ;;
+    *) 
+        vim "$IN"
+        echo -n "vim \"$IN\"" | xclip -selection primary
+        ;;
+esac
