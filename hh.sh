7d7c4ae87df637c21ed00beec0b410277203c8cd
4tila <loboatila@gmail.com>
2025-11-10 10:40:12 -0300

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -1,1 +1,2 @@
-history | fzf --tac --no-sort | awk '{for (i=2; i<=NF; i++) printf "%s ", $i; print ""}' |  xclip -selection primary
+str="$(history | fzf --tac --no-sort | awk '{for (i=2; i<=NF; i++) printf "%s ", $i; print ""}')"
+echo -n $str |  xclip -selection primary
